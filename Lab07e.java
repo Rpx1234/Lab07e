f05d3167edaa26dd01b1d8044de0ac68fce45855
Rpx1234 <22777764+Rpx1234@users.noreply.github.com>
2019-01-31 11:24:05 -0600

    Add files via upload

---
+++ Lab07e.java
@@ -0,0 +1,41 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+
+public class Lab07e
+{
+ public static void main( String args[] )
+ {
+   
+      MorseCode test = new MorseCode('A');
+      out.println(test);
+      out.println();
+      
+      test.setChar('B');
+      out.println(test);
+      
+      test.setChar('3');
+      out.println(test);
+      
+      test.setChar('Z');
+      out.println(test);
+      
+      test.setChar('8');
+      out.println(test);
+      
+      test.setChar('0');
+      out.println(test);
+        
+  
+      
+  //add test cases
+  
+  
+  
+ }
+}
